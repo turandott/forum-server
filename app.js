0b342b68181a7ba90203150559934b6d8b5aaf11
turandott <greyvilka@gmail.com>
2023-05-27 22:08:00 +0300

    Add swagger

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const sequelize = require('./config/dbConfig');
 const bodyparser = require('body-parser');
 const User = require('./models/User');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 
 
 const app = express();
@@ -12,11 +15,25 @@ const corsOption = {
 }
 
 
-//middleware
-app.use(cors(corsOption))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Forum API',
+      version: '1.0.0',
+    }
+  },
+  apis: ['app.js'],
+};
 
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+// console.log(swaggerDocs);
+
+
+//middleware
+app.use(cors(corsOption));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //testing api
 
@@ -33,6 +50,30 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`server running in port ${PORT}`)
 })
+
+
+//swagger
+
+/**
+ * @swagger
+ * /users:
+ *  get:
+ *    description: Get all users
+ *    responses:
+ *      200:
+ *        description: Success
+ */
+app.get('/users', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Smith',
+      password: '1234567',
+
+    }
+  ])
+});
 
 
 //CRUD routes
